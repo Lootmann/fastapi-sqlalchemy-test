a60ad2452b138bcdf9fea0b0a45b5e99be4d2716
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-12 19:34:07 +0900

    src/db: fix declarative_base order

--- src/db.py
+++ src/db.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 DATABASE_URL = "sqlite:///dev.db"
 
 engine = create_engine(DATABASE_URL, echo=True)
 session = sessionmaker(bind=engine)
+
+Base = declarative_base()
 
 
 def get_db():
