9f80e13d8c34ee2e3d96f64bf250e9e2504062a1
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-13 10:09:54 +0900

    tests/post: create post, and get post by user_id and post_id

--- tests/test_post.py
+++ tests/test_post.py
@@ -33,11 +33,10 @@ class TestCreatePost:
         resp = client.post(f"/users/{user.id}/posts", json=data)
         assert resp.status_code == status.HTTP_200_OK
 
-        post = post_schema.Post(**resp.json())
+        post = post_schema.PostCreateResponse(**resp.json())
         assert post.id == 1
-        assert post.title == data["title"]
-        assert post.content == data["content"]
-        assert post.user_id == user.id
+        assert post.title == "hello"
+        assert post.content == "world"
 
     def test_create_post_no_exists_user_id(self, client):
         # create post
@@ -49,7 +48,7 @@ class TestCreatePost:
         assert resp.status_code == status.HTTP_404_NOT_FOUND
 
 
-class TestGetpost:
+class TestGetPost:
     def test_get_no_posts(self, client):
         # create user
         resp = client.post("/users", json={"name": "hige"})
@@ -68,16 +67,42 @@ class TestGetpost:
 
         # create post
         data = {
-            "title": "hello",
-            "content": "world",
+            "title": "this is a title",
+            "content": "what is content :^)",
         }
         resp = client.post(f"/users/{user.id}/posts", json=data)
-        post = post_schema.Post(**resp.json())
+        post_json = resp.json()
+
+        assert post_json["id"] == 1
+        assert post_json["title"] == "this is a title"
+        assert post_json["content"] == "what is content :^)"
+
+        # test user has a post
+        resp = client.get(f"/users/{user.id}")
+        user_obj = user_schema.User(**resp.json())
+
+        assert user_obj.posts[0].id == post_json["id"]
+        assert user_obj.posts[0].title == post_json["title"]
+        assert user_obj.posts[0].content == post_json["content"]
+
+    def test_get_post_by_post_id(self, client):
+        # create user
+        resp = client.post("/users", json={"name": "hage"})
+        user = user_schema.User(**resp.json())
+
+        # create post
+        data = {
+            "title": "tt",
+            "content": "cc",
+        }
+        resp = client.post(f"/users/{user.id}/posts", json=data)
+        post_json = resp.json()
+
+        # get post by post_id
+        resp = client.get(f"/users/{user.id}/posts/{post_json['id']}")
         assert resp.status_code == status.HTTP_200_OK
 
-        # get post by user_id and post_id
-        resp = client.get(f"/users/{user.id}")
-        print(resp.json())
-
-        print(user)
-        print(post)
+        post_obj = post_schema.Post(**resp.json())
+        assert post_obj.user_id == user.id
+        assert post_obj.title == data["title"]
+        assert post_obj.content == post_json["content"]
