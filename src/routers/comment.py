92c995e5fcbe8f2e58f83a5541f659e0b871c99c
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-14 14:52:37 +0900

    routers: rewrite all routers

--- src/routers/comment.py
+++ src/routers/comment.py
@@ -1,3 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from src.db import get_db
+
 
 router = APIRouter()
+
+
+@router.get("/comments")
+def get_all_comments(db: Session = Depends(get_db)):
+    return {"router comments": "GET /comments"}
+
+
+@router.get("/comments/{comment_id}")
+def get_comment_by_id(comment_id: int, db: Session = Depends(get_db)):
+    return {"router comments": f"GET /comments/{comment_id}"}
+
+
+@router.post("/comments")
+def create_comment(db: Session = Depends(get_db)):
+    return {"router comments": "POST/comments"}
--- src/routers/user.py
+++ src/routers/user.py
@@ -1,63 +1,41 @@
-from typing import List
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from src.db import get_db
-from src.schemas import user as user_schema
-from src.schemas import post as post_schema
-from src.apis import user as user_api
-from src.apis import post as post_api
 
 router = APIRouter()
 
 
-@router.get("/status")
-def server_status():
-    return {"status": "seems fine :^)"}
-
-
-@router.get("/users", response_model=List[user_schema.User])
+@router.get("/users")
 def get_all_users(db: Session = Depends(get_db)):
-    return user_api.get_all_users(db)
+    return {"router users": "get all users"}
 
 
-@router.get("/users/{user_id}", response_model=user_schema.User)
-def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
-    result = user_api.get_user_by_id(db, user_id)
-    if not result:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User {user_id} not found",
-        )
-    return result
+@router.get("/users/{user_id}")
+def get_post_by_id(user_id: int, db: Session = Depends(get_db)):
+    return {"router users": f"GET /users/{user_id}"}
 
 
-@router.post("/users", response_model=user_schema.UserCreateResponse)
-def create_user(user_body: user_schema.UserCreate, db: Session = Depends(get_db)):
-    return user_api.create_user(db, user_body)
-
-
-@router.get("/users/{user_id}/posts", response_model=List[post_schema.Post])
+@router.get("/users/{user_id}/posts")
 def get_all_posts_by_user(user_id: int, db: Session = Depends(get_db)):
-    result = user_api.get_user_by_id(db, user_id)
-    if not result:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User {user_id} not found",
-        )
-    return post_api.get_all_posts_by_user(db, user_id)
+    return {"router users": f"GET /users/{user_id}/posts"}
 
 
-@router.post("/users/{user_id}/posts", response_model=post_schema.PostCreateResponse)
-def create_post(
-    user_id: int,
-    post_body: post_schema.PostCreate,
-    db: Session = Depends(get_db),
-):
-    result = user_api.get_user_by_id(db, user_id)
-    if not result:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User {user_id} not found",
-        )
-    return post_api.create_post(db, user_id, post_body)
+@router.get("/users/{user_id}/posts/{post_id}")
+def get_post_by_user(user_id: int, post_id: int, db: Session = Depends(get_db)):
+    return {"router users": f"GET /users/{user_id}/posts/{post_id}"}
+
+
+@router.get("/users/{user_id}/comments")
+def get_all_comments_by_user(user_id: int, db: Session = Depends(get_db)):
+    return {"router users": f"GET /users/{user_id}/comments"}
+
+
+@router.get("/users/{user_id}/comments/{comment_id}")
+def get_comment_by_user(user_id: int, comment_id: int, db: Session = Depends(get_db)):
+    return {"router users": f"GET /users/{user_id}/comments/{comment_id}"}
+
+
+@router.post("/users")
+def create_user(db: Session = Depends(get_db)):
+    return {"router users": f"POST /users/"}
