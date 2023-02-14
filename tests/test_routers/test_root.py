5f0ed18813decc1641662a72aa4d1bd4f31a5210
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-14 15:05:51 +0900

    routers: add test endpoint, and test it

---
+++ tests/test_routers/test_root.py
@@ -0,0 +1,11 @@
+from fastapi import status
+from tests.init_client import test_client as client
+
+
+class TestCreatePost:
+    def test_root(self, client):
+        resp = client.get("/ping")
+        assert resp.status_code == status.HTTP_200_OK
+
+        resp_obj = resp.json()
+        assert resp_obj["ping"] == "pong :^)"
