ea784372c6aff12716492b0795ccc0268a464458
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-18 00:04:41 +0900

    tests: login and create token and required JWT token post test :^)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.schemas import auth as auth_schema
 from src.schemas import post as post_schema
 from src.schemas import user as user_schema
 from tests.init_client import test_client as client
@@ -8,20 +9,36 @@ from tests.util import random_string
 
 @pytest.fixture
 def user_fixture(client):
-    resp = client.post("/users", json={"name": random_string(), "password": random_string()})
+    username = random_string()
+    password = random_string()
+    resp = client.post("/users", json={"name": username, "password": password})
+
+    token_resp = client.post(
+        "/token",
+        data={"username": username, "password": password},
+        headers={"content-type": "application/x-www-form-urlencoded"},
+    )
+    token_json = token_resp.json()
+    token = auth_schema.Token(**token_json)
+
     return user_schema.UserCreateResponse(**resp.json())
 
 
 @pytest.fixture
 def post_fixture(client):
+    username = random_string()
+    password = random_string()
     resp = client.post(
         "/users",
-        json={
-            "name": random_string(),
-            "password": random_string(),
-        },
+        json={"name": username, "password": password},
     )
     user = user_schema.UserCreateResponse(**resp.json())
+
+    login_resp = client.post(
+        "/token",
+        data={"username": username, "password": password},
+        headers={"content-type": "application/x-www-form-urlencoded"},
+    )
 
     resp = client.post(
         "/posts",
