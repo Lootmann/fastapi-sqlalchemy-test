808e37a02192f0b74d4425c1f74b8f96b6b2222c
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-17 19:29:04 +0900

    auth: simple jwt authentication - login api

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 
+from src.routers import auth as auth_router
 from src.routers import comment as comment_router
 from src.routers import post as post_router
 from src.routers import root as root_router
 from src.routers import user as user_router
 
 app = FastAPI()
+
 app.include_router(root_router.router)
 app.include_router(user_router.router)
 app.include_router(post_router.router)
 app.include_router(comment_router.router)
+app.include_router(auth_router.router)
---
+++ src/routers/auth.py
@@ -0,0 +1,31 @@
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from src.apis import auth as auth_api
+from src.db import get_db
+from src.schemas import auth as auth_schema
+from src.settings import Settings
+
+router = APIRouter()
+
+
+@router.post("/token", response_model=auth_schema.Token)
+def login(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+    user = auth_api.authenticate_user(db, form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Incorrect input data",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=Settings.access_token_expire_minutes)
+
+    data = {"sub": user.name}
+    access_token = auth_api.create_access_token(data, access_token_expires)
+
+    return {"access_token": access_token, "token_type": "bearer"}
